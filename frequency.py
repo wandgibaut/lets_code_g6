f1007096c7733e2665b1d2a98b47f132b60dc05e
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-11-13 21:04:19 -0300

    Novo upgrade na função frequency

--- frequency.py
+++ frequency.py
@@ -7,29 +7,41 @@ class Frequency:
         self.data = data.select_dtypes(np.object_)
         self.columns = self.data.columns
     
-    def get_frequency(self, column: str = 'column') -> pd.DataFrame:
+
+    def show(self):
         
-        print (column not in self.columns)
+        report = {}
+        
+        for column in self.columns:
+
+            report[column] = self.__get_frequency__(column)
+            
+
+        return report
+
+
+    def __get_frequency__(self, column: str = 'column') -> pd.DataFrame:
+        
         if column not in self.columns:
             raise Exception("ColumnNotIsQualitative")
 
         self.frequency = pd.DataFrame()
         self.serie = self.data[column]
-        self.__absolute__()
-        self.__relative__()
-        self.__absolute_accumulated__()
-        self.__relative_accumulated__()
+        self.frequency['Absolute'] = self.__absolute__()
+        self.frequency['Relative'] = self.__relative__()
+        self.frequency['Absolute accumulated'] = self.__absolute_accumulated__()
+        self.frequency['Relative accumulated'] = self.__relative_accumulated__()
         return self.frequency
         
-    def __absolute__(self) -> None:
-        self.frequency['Absolute'] = self.serie.value_counts()
+    def __absolute__(self) -> pd.Series:
+       return self.serie.value_counts()
     
-    def __relative__(self) -> None:
-        self.frequency['Relative'] = self.frequency['Absolute']/self.serie.count()
+    def __relative__(self) -> pd.Series:
+        return self.frequency['Absolute']/self.serie.count()
     
-    def __absolute_accumulated__(self) -> None:
-        self.frequency['Absolute accumulated'] = self.frequency['Absolute'].cumsum()
+    def __absolute_accumulated__(self) -> pd.Series:
+        return self.frequency['Absolute'].cumsum()
         
-    def __relative_accumulated__(self) -> None:
-        self.frequency['Relative accumulated'] = self.frequency['Relative'].cumsum()
+    def __relative_accumulated__(self) -> pd.Series:
+        return self.frequency['Relative'].cumsum()
         

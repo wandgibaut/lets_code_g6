a1c8d86b07d26b7bed7a29dbf9a7271c35d3b813
Joao Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-11-20 15:33:16 -0300

    Formatting and making private methods with only one leading underscore

--- frequency.py
+++ frequency.py
@@ -1,47 +1,44 @@
 import numpy as np
 import pandas as pd
 
+
 class Frequency:
 
-    def __init__(self, data:pd.DataFrame, columns = pd.Series):
+    def __init__(self, data: pd.DataFrame, columns=pd.Series):
         self.data = data
         self.columns = columns
-    
 
     def show(self):
-        
+
         report = {}
-        
+
         for column in self.columns:
 
-            report[column] = self.__get_frequency__(column)
-            
+            report[column] = self._get_frequency(column)
 
         return report
 
+    def _get_frequency(self, column: str = 'column') -> pd.DataFrame:
 
-    def __get_frequency__(self, column: str = 'column') -> pd.DataFrame:
-        
         if column not in self.columns:
             raise Exception("ColumnNotIsQualitative")
 
         self.frequency = pd.DataFrame()
         self.serie = self.data[column]
-        self.frequency['Absolute'] = self.__absolute__()
-        self.frequency['Relative'] = self.__relative__()
-        self.frequency['Absolute accumulated'] = self.__absolute_accumulated__()
-        self.frequency['Relative accumulated'] = self.__relative_accumulated__()
+        self.frequency['Absolute'] = self._absolute()
+        self.frequency['Relative'] = self._relative()
+        self.frequency['Absolute accumulated'] = self._absolute_accumulated()
+        self.frequency['Relative accumulated'] = self._relative_accumulated()
         return self.frequency
-        
-    def __absolute__(self) -> pd.Series:
-       return self.serie.value_counts()
-    
-    def __relative__(self) -> pd.Series:
+
+    def _absolute(self) -> pd.Series:
+        return self.serie.value_counts()
+
+    def _relative(self) -> pd.Series:
         return self.serie.value_counts(normalize=True)
-    
-    def __absolute_accumulated__(self) -> pd.Series:
+
+    def _absolute_accumulated(self) -> pd.Series:
         return self.frequency['Absolute'].cumsum()
-        
-    def __relative_accumulated__(self) -> pd.Series:
+
+    def _relative_accumulated(self) -> pd.Series:
         return self.frequency['Relative'].cumsum()
-        
